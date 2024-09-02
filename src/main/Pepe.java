be11b07ff0d18ea1eb8efa13b030da593c3182da
Mochila <geibrus.00@gmail.com>
2024-09-02 23:57:46 +0200

    Primer commit

---
+++ src/main/Pepe.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Pepe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame ventana = new Frame();
+		
+		System.out.println("Antes de sacar ventana");
+		ventana.run();
+		System.out.println("Despues de sacar ventana");
+	}
+
+}
